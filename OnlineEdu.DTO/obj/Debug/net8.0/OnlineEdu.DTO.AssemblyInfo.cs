34125058aec0dbb012e28a0419e25732eec5d14c
Latif Altay <latifaltay00@gmail.com>
2024-08-23 12:50:21 +0300

    Entities are updated

--- OnlineEdu.DTO/obj/Debug/net8.0/OnlineEdu.DTO.AssemblyInfo.cs
+++ OnlineEdu.DTO/obj/Debug/net8.0/OnlineEdu.DTO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineEdu.DTO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adeea147528134efe630c89682bdd4a7c75aa811")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineEdu.DTO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineEdu.DTO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
