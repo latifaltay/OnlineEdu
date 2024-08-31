77ffc1da8df3deb8f1b084ef4fa70eaaf30afedc
Latif Altay <latifaltay00@gmail.com>
2024-09-01 01:43:59 +0300

    The course categories that are active are listed

--- OnlineEdu.API/obj/Debug/net8.0/OnlineEdu.API.AssemblyInfo.cs
+++ OnlineEdu.API/obj/Debug/net8.0/OnlineEdu.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineEdu.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a753bce027c07bbda89b6f38c06493b9c14c3c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ba7723a10a0492f9aeddff2033d96ea0fe0da72")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineEdu.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineEdu.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ OnlineEdu.WebUI/ViewComponents/Home/_HomeCourseCategoryComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineEdu.WebUI.DTOs.CourseCategoryDtos;
+using OnlineEdu.WebUI.Helpers;
+
+namespace OnlineEdu.WebUI.ViewComponents.Home
+{
+    public class _HomeCourseCategoryComponent: ViewComponent
+    {
+        private readonly HttpClient _clinet = HttpClientInstance.CreateClient();
+        public async Task<IViewComponentResult> InvokeAsync() 
+        {
+            var values = await _clinet.GetFromJsonAsync<List<ResultCourseCategoryDto>>("CourseCategories/GetActiveCategories");
+            return View(values);
+        }
+
+    }
+}
