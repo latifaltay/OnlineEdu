281a10c7a31fdc77e4dd2f807ceeee3262596797
Latif Altay <latifaltay00@gmail.com>
2024-09-01 23:57:56 +0300

    register theme and custom errors are created

--- OnlineEdu.WebUI/DTOs/UserDtos/UserRegisterDto.cs
+++ OnlineEdu.WebUI/DTOs/UserDtos/UserRegisterDto.cs
@@ -1,4 +1,6 @@
-﻿namespace OnlineEdu.WebUI.DTOs.UserDtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineEdu.WebUI.DTOs.UserDtos
 {
     public class UserRegisterDto
     {
@@ -7,6 +9,7 @@
         public string UserName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        [Compare("Password", ErrorMessage ="Parolalar Eşleşmiyor!")]
         public string ConfirmPassword { get; set; }
     }
 }
--- OnlineEdu.WebUI/obj/Debug/net8.0/OnlineEdu.WebUI.AssemblyInfo.cs
+++ OnlineEdu.WebUI/obj/Debug/net8.0/OnlineEdu.WebUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineEdu.WebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c56d5c5d1a6a3fd6782cee9e3864d0a429949e30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1877df1726661ef927a858ab44c177aa29d5607f")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineEdu.WebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineEdu.WebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
